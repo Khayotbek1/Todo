62b186e477b601586db0903e00684eabcbff867a
Khayot Razzakov <hrazzoqov0002@gmail.com>
2025-03-31 19:32:50 +0500

    create task section

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,17 @@
-
 from django.contrib import admin
 from django.urls import path
+from main.views import *
 
-from main.views import students_view, home_view, task_view, not_done, course_3
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('students/', students_view,  name = 'students'),
     path('tasks/', task_view, name='tasks'),
+    path('tasks/create-task/', create_task, name='create-task'),
     path('tasks/not_done/', not_done, name='not_done'),
-    path('students/course_3/', course_3, name='course_3')
+    path('tasks/<int:pk>/delete-confirm/', task_delete_confirm, name='delete-confirm'),
+    path('tasks/<int:pk>/delete/', task_delete, name='task-delete'),
+    path('students/course_3/', course_3, name='course_3'),
+    path('students/age_20/', age_20, name='age_20'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.template.context_processors import request
+from django.template.loader import render_to_string
+
 from .models import *
 
 def home_view(request):
     home = Student.objects.all()
 
     return render(request, 'home.html', {'home': home})
-
 
 def students_view(request):
     students = Student.objects.all()
@@ -18,13 +20,42 @@ def students_view(request):
 
     return render(request, 'students.html', context)
 
-
 def task_view(request):
     task = Task.objects.all()
     context = {
         'tasks': task
     }
     return render(request, 'tasks.html', context)
+
+def create_task(request):
+    if request.method == 'POST':
+        Task.objects.create(
+            name = request.POST.get('vazifa'),
+            student = Student.objects.get(id = request.POST.get('student_id')),
+            date = request.POST.get('sana'),
+            desc = request.POST.get('batafsil'),
+        )
+
+        return redirect('tasks')
+
+    student = Student.objects.all()
+    context = {
+        'students': student,
+    }
+
+    return render(request, 'create_task.html', context)
+
+def task_delete_confirm(request, pk):
+    task = Task.objects.get(id = pk)
+    context = {
+        'task' : task,
+    }
+    return render(request, 'task-delete-confirm.html', context)
+
+def task_delete(request, pk):
+    task = Task.objects.get(id = pk)
+    task.delete()
+    return redirect('tasks')
 
 def not_done(request):
     task = Task.objects.filter(done = False)
@@ -39,3 +70,10 @@ def course_3(request):
         'courses': course,
     }
     return render(request, 'course3.html', context )
+
+def age_20(request):
+    age = Student.objects.filter(age__gt = 20)
+    context = {
+        'ages': age
+    }
+    return render(request, 'age_20.html', context)
